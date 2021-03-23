572767d15bbc40aa5d6511eb2bdf298cde289779
Sai <sai@PurpleTalks-MacBook-Air.local>
2021-03-24 01:15:49 +0530

    Implemented JWT token authorization

---
+++ src/shared/services/jwt-auth/jwt-auth.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { SECRET_KEY_JSON_WEB_TOKEN } from 'src/shared/enums/app.properties';
+import { JwtAuthService } from './jwt-auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt-strategy';
+
+@Module({
+    imports: [
+        PassportModule.register({defaultStrategy: 'jwt'}),
+        JwtModule.register({
+            secret: SECRET_KEY_JSON_WEB_TOKEN,
+            signOptions: { expiresIn: '300s' },
+          }),
+    ],
+    providers: [JwtAuthService, JwtStrategy],
+    exports: [JwtModule, JwtAuthService, PassportModule]
+})
+export class JwtAuthModule {}

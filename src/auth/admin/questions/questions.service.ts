93b43fd0e15f1c2ba3e3389b9a844c483b99beb9
sai korthivada <sai.korthivada@xcubelabs.com>
2021-08-07 23:09:09 +0530

    Updated exceptions in questions and technologies

--- src/auth/admin/questions/questions.service.ts
+++ src/auth/admin/questions/questions.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { MODAL_ENUMS } from 'src/shared/enums/models.enums';
 import { CreateQuestionDto } from './questions.dto';
@@ -8,29 +8,54 @@ import { IQuestionDocument } from './questions.schema';
 export class QuestionsService {
     constructor(
         @Inject(MODAL_ENUMS.QUESTIONS) private readonly questionsModel: Model<IQuestionDocument>,
-    ) {}
+    ) { }
 
-    async createQuestions(questionsPayload: CreateQuestionDto): Promise<CreateQuestionDto> {
-        const question = new this.questionsModel(questionsPayload);
-        return question.save();
-    }
-
-    async getAllQuestions(): Promise<CreateQuestionDto[]> {
-        const questionDetails = await this.questionsModel.find().select('-answerKey').exec();
-        return questionDetails;
-    }
-
-    async deleteQuestion(questionId: string): Promise<IQuestionDocument | NotFoundException> {
-        const result = await this.questionsModel.findByIdAndDelete(questionId).exec();
-        return result;
-    }
-
-    async updateQuestion(questionsPayload: CreateQuestionDto, questionId: string): Promise<CreateQuestionDto> {
-        const questionDetails = await this.questionsModel.findOneAndUpdate({_id: questionId}, questionsPayload).exec();
-        if (!questionDetails) {
-            throw new HttpException('Nothing has changed', HttpStatus.NOT_MODIFIED);
+    async createQuestions(questionsPayload: CreateQuestionDto): Promise<CreateQuestionDto | UnprocessableEntityException> {
+        try {
+            const question = new this.questionsModel(questionsPayload);
+            return question.save();
         }
-        return questionsPayload;
+        catch (e) {
+            throw new HttpException(`Something went wrong ... Please try again`, HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+    }
+
+    async getAllQuestions(): Promise<CreateQuestionDto[] | UnprocessableEntityException> {
+        try {
+            const questionDetails = await this.questionsModel.find().select('-answerKey').exec();
+            return questionDetails;
+        }
+        catch (e) {
+            throw new HttpException(`Something went wrong ... Please try again`, HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+
+    }
+
+    async deleteQuestion(questionId: string): Promise<IQuestionDocument | NotFoundException | UnprocessableEntityException> {
+        try {
+            const result = await this.questionsModel.findByIdAndDelete(questionId).exec();
+            return result;
+        }
+        catch (e) {
+            throw new HttpException(`Something went wrong ... Please try again`, HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+    }
+
+    async updateQuestion(questionsPayload: CreateQuestionDto, questionId: string): Promise<CreateQuestionDto | UnprocessableEntityException | NotFoundException> {
+        try {
+            const questionDetails = await this.questionsModel.findOneAndUpdate({ _id: questionId }, questionsPayload).exec();
+            if (!questionDetails) {
+                throw new HttpException('Nothing has changed', HttpStatus.NOT_MODIFIED);
+            }
+            return questionsPayload;
+        }
+        catch (e) {
+            throw new HttpException(`Something went wrong ... Please try again`, HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
     }
 
 
--- src/auth/admin/technology/technology.service.ts
+++ src/auth/admin/technology/technology.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException, HttpStatus, UnprocessableEntityException, HttpException } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { MODAL_ENUMS } from 'src/shared/enums/models.enums';
 import { Utils } from 'src/shared/services/Utils/Utils';
@@ -11,25 +11,48 @@ export class TechnologyService {
         @Inject(MODAL_ENUMS.TECHNOLOGIES) private readonly technologyModel: Model<ITechnologyDocument>,
     ) {}
 
-    async createTechnology(technologyPayload: CreateTechnologyDto): Promise<CreateTechnologyDto> {
-        technologyPayload.technologyKey = Utils.replaceAllSpecialCharactersWithHipens(technologyPayload.name);
-        const technology = new this.technologyModel(technologyPayload);
-        return technology.save();
+    async createTechnology(technologyPayload: CreateTechnologyDto): Promise<CreateTechnologyDto | UnprocessableEntityException> {
+        try {
+            technologyPayload.technologyKey = Utils.replaceAllSpecialCharactersWithHipens(technologyPayload.name);
+            const technology = new this.technologyModel(technologyPayload);
+            return technology.save();
+        }
+        catch (e) {
+            throw new HttpException(`Something went wrong ... Please try again`, HttpStatus.UNPROCESSABLE_ENTITY);
+        }
     }
 
-    async getAllTechnologies(): Promise<CreateTechnologyDto[]> {
-        const technologyDetails = await this.technologyModel.find({}).exec();
-        return technologyDetails;
+    async getAllTechnologies(): Promise<CreateTechnologyDto[] | UnprocessableEntityException> {
+        try {
+            const technologyDetails = await this.technologyModel.find({}).exec();
+            return technologyDetails;
+        }
+        catch (e) {
+            throw new HttpException(`Something went wrong ... Please try again`, HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+      
     }
 
-    async deleteTechnology(technologyId: string): Promise<ITechnologyDocument | NotFoundException> {
-        const result = await this.technologyModel.findByIdAndDelete(technologyId).exec();
-        return result;
+    async deleteTechnology(technologyId: string): Promise<ITechnologyDocument | NotFoundException | UnprocessableEntityException> {
+        try {
+            const result = await this.technologyModel.findByIdAndDelete(technologyId).exec();
+            return result;
+        }
+        catch (e) {
+            throw new HttpException(`Something went wrong ... Please try again`, HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+      
     }
 
-    async getTechnologyById(technologyId: string): Promise<ITechnologyDocument[] | NotFoundException> {
-        const result = await this.technologyModel.find({technologyKey: technologyId}).exec();
-        return result;
+    async getTechnologyById(technologyId: string): Promise<ITechnologyDocument[] | NotFoundException | UnprocessableEntityException> {
+        try {
+            const result = await this.technologyModel.find({technologyKey: technologyId}).exec();
+            return result;
+        }
+        catch (e) {
+            throw new HttpException(`Something went wrong ... Please try again`, HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+       
     }
 
 }

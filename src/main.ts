aa8802bb89ed2fd1082a379bba1943b500e828e2
sai korthivada <sai.korthivada@xcubelabs.com>
2021-10-06 13:35:27 +0530

    Updated the payload size

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 /* eslint-disable prettier/prettier */
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { json } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  app.use(json({ limit: '30mb' }));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
